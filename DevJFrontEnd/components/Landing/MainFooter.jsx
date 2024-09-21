6513bd2127458262e90de350c232eed4248caa64
Chetakk <kumarchetak47@gmail.com>
2024-09-21 15:16:21 +0530

    Terms Page

---
+++ DevJFrontEnd/components/Landing/MainFooter.jsx
@@ -0,0 +1,17 @@
+import "../../styles/MainFooter.css";
+
+export default function MainFooter() {
+
+
+    return(
+        <div className="fbar">
+            <div className="contacts">
+
+            </div>
+            <p className="para">
+
+            </p>
+            <img src="/footer.png"></img>
+        </div>
+    );
+}
