d953116ac95f0cfbf7095891b5780cad25e7d946
Zonde246 <72788587+Zonde246@users.noreply.github.com>
2024-09-22 08:20:54 +0530

    Fix Cart

--- DevJFrontEnd/components/ProductCard.jsx
+++ DevJFrontEnd/components/ProductCard.jsx
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import '../styles/ProductCard.css';
+import React, { useState } from "react";
+import "../styles/ProductCard.css";
 
 const ProductCard = ({ product }) => {
   const [quantity, setQuantity] = useState(1);
 
   const handleIncrement = () => {
-    setQuantity(prevQuantity => prevQuantity + 1);
+    setQuantity((prevQuantity) => prevQuantity + 1);
   };
 
   const handleDecrement = () => {
     if (quantity > 1) {
-      setQuantity(prevQuantity => prevQuantity - 1);
+      setQuantity((prevQuantity) => prevQuantity - 1);
     }
   };
 
   const handleDelete = () => {
     // Logic to remove item from cart
-    console.log('Item removed from cart');
+    console.log("Item removed from cart");
   };
 
   return (
     <div className="product-card-container">
-      <img src={product.image} alt={product.name} className="product-card-image" />
+      <img
+        src={product.imgurl}
+        alt={product.ProductName}
+        className="product-card-image"
+      />
       <div className="product-card-details">
-      <div className="product-card-title">{product.name}</div>
-      <div className="product-card-description">{product.description}</div>
+        <div className="product-card-title">{product.ProductName}</div>
+        <div className="product-card-description">{product.Description}</div>
       </div>
       <div className="product-price-delete">
-      <div className="product-card-price">${product.price}</div>
-        <button onClick={handleDelete} className="delete-btn">🗑️</button>
+        <div className="product-card-price">${product.Rate}</div>
+        <button onClick={handleDelete} className="delete-btn">
+          🗑️
+        </button>
       </div>
     </div>
   );
